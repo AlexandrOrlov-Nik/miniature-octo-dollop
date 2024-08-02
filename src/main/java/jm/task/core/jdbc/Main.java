edfb39c603d67e5dfcf11a5c0284f91addfa1864
AlexandorOrlov <oalexandr248@gmail.com>
2024-08-02 21:43:13 +0300

    НЯ
    (cherry picked from commit 0fd5dd184d165d17d9a2f0cf62f366c6f96a491c)

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,16 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserDaoJDBCImpl userr = new UserDaoJDBCImpl();
+        userr.createUsersTable();
+        userr.saveUser("Ivan", "Ogr", (byte) 33);
+        List<User> list = userr.getAllUsers();
+        System.out.println();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,7 @@
 package jm.task.core.jdbc.util;
 
+import org.hibernate.boot.model.relational.Database;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    /* реализуйте настройку соеденения с БД */
 }
